c610904d97fe9556e04540dc595aa2d22c462868
ParshantGoyal4 <goyalparshant564@gmail.com>
2025-01-17 08:40:52 +0000

    abortcontroller setup

--- src/components/Subcomponents/PostList.jsx
+++ src/components/Subcomponents/PostList.jsx
@@ -14,11 +14,18 @@ const PostList =()=>{
   // }
     useEffect(()=>{
       setdatafetched(true);
-      fetch("https://dummyjson.com/posts")
+      const controller = new AbortController();
+      const signal = controller.signal;
+
+      fetch("https://dummyjson.com/posts",{signal})
        .then((res)=> res.json())
          .then((data) =>{addInitialPosts(data.posts)
           setdatafetched(false);
          });
+         return()=>{
+          console.log("abort");
+          controller.abort();
+         };
     },[])
 
   const handleGetPostsClick =()=>{
